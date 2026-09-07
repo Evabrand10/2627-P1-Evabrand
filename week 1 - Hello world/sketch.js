41bc1d6e41e03b27fdd35a9c0736b9de98753f69
Evabrand10 <davelamberts2010@icloud.com>
2026-09-07 12:20:36 +0200

    dag 1

--- week 1 - Hello world/sketch.js
+++ week 1 - Hello world/sketch.js
@@ -1,7 +1,17 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
-}
+  background (367);
+
+  textSize(32);
+  text("Eva Lamberts", 50, 100);
+  textStyle(BOLD);
+
+  rect(50, 150, 200, 110);
+    fill(255);
+  rect(50, 110, 200, 50);
+ fill(225,0,0);
+  rect(50, 160, 200, 50);
+} 
