1b11dc91c2bc987bf07170685a882e634713d307
Evabrand10 <davelamberts2010@icloud.com>
2026-09-08 12:16:05 +0200

    08/09/2026

--- week 1 - Hello world/sketch.js
+++ week 1 - Hello world/sketch.js
@@ -1,17 +1,63 @@
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(1960, 800);
 }
 
 function draw() {
   background (367);
-fill (0);
- textSize(32);
+ 
+  fill ("pink");
+ textSize(26);
  text('Eva Lamberts', 50, 50);
  textStyle(BOLD); 
-  fill(0, 0, 255);
+ //makes my name appear on the canvas
+  fill(0, 0, 255);//shows the color blue 
   rect(50, 150, 200, 110);
-  fill(255, 0, 0);
+  fill(255, 0, 0); //shows the color red
   rect(50, 110, 200, 50);
+  fill(255);// shows the color white
+  rect(50, 160, 200, 50); 
+  //makes the dutch flag    
+  stroke(0);
+  strokeWeight(4);
+  fill(0);
+  rect (50,300, 300,300); 
+  noStroke();
   fill(255);
-  rect(50, 160, 200, 50);
+  rect (50, 300, 37.5, 37.5);
+  rect (125, 300, 37.5, 37.5);
+  rect (200, 300, 37.5, 37.5);
+  rect (275, 300, 37.5, 37.5);
+  rect (87.5, 337.5, 37.5, 37.5);
+  rect (162.5, 337.5, 37.5, 37.5);
+  rect (237.5, 337.5, 37.5, 37.5);
+  rect (312.5, 337.5, 37.5, 37.5);
+  rect (50, 375, 37.5, 37.5);
+  rect (125, 375, 37.5, 37.5);
+  rect (200, 375, 37.5, 37.5);
+  rect (275, 375, 37.5, 37.5);
+  rect (87.5, 412.5, 37.5, 37.5);
+  rect (162.5, 412.5, 37.5, 37.5);
+  rect (237.5, 412.5, 37.5, 37.5);
+  rect (312.5, 412.5, 37.5, 37.5);
+  rect (50, 450, 37.5, 37.5);
+  rect (125, 450, 37.5, 37.5);
+  rect (200, 450, 37.5, 37.5);
+  rect (275, 450, 37.5, 37.5);
+  rect (87.5, 487.5, 37.5, 37.5);
+  rect (162.5, 487.5, 37.5, 37.5);
+  rect (237.5, 487.5, 37.5, 37.5);
+  rect (312.5, 487.5, 37.5, 37.5);
+  rect (50, 525, 37.5, 37.5);
+  rect (125, 525, 37.5, 37.5);
+  rect (200, 525, 37.5, 37.5);
+  rect (275, 525, 37.5, 37.5);
+  rect (87.5, 562.5, 37.5, 37.5);
+  rect (162.5, 562.5, 37.5, 37.5);
+  rect (237.5, 562.5, 37.5, 37.5);
+  rect (312.5, 562.5, 37.5, 37.5);
+ //chessboard
+  stroke(0)
+  strokeWeight(2)
+  rect(300,100,60,60)
+  triangle(300,100,330,50,360,100);
 }
