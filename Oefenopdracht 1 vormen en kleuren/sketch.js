c021122379b9649e165958b1fbc006141a15b907
Evabrand10 <davelamberts2010@icloud.com>
2026-09-08 11:52:39 +0200

    Oefen opdracht 1

---
+++ Oefenopdracht 1 vormen en kleuren/sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(220);
+ strokeWeight(4);
+ fill("blue");
+ rect(0,100,75,200);
+ fill("white");
+ rect(0,0,150,100);
+ fill("red");
+ rect(150,0,75,200);
+ fill("white");
+ rect(75,100,75,225);
+ fill("red");
+ rect(0,300,75,100);
+ fill("yellow");
+ rect(75,325,200,75);
+ fill("blue");
+ rect(150,200,225,125);
+ fill("white  ");
+ rect(275,325,100,75);
+  fill("red");
+ rect(350,200,75,200);
+ fill("white");
+ rect(425,275,175,125);
+ fill("yellow");
+ rect(225,0,200,75);
+ fill("white");
+ rect(225,75,200,125);
+ fill("yellow");
+rect(425,200,175,75);
+fill("blue");
+rect(425,0,175,200);
+ 
+}
+
+
