82641a8fab3d518333c24791002298aed6cac6c3
Evabrand10 <davelamberts2010@icloud.com>
2026-09-08 14:29:29 +0200

    safe file

---
+++ lege sketch 2/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 400);
+ //makes the canvas 600 pixels wide and 400 pixels high
+}
+
+
+function draw() {
+  background (367);
+  circle(300, 200, 100);
+  rect(275, 250, 50, 100);
+  ellipse(270, 200, 25, 50); 
+  ellipse(330, 200, 25, 50);
+  line(325, 350, 350, 370);
+  line(275, 350, 250, 370);
+ 
+}
--- week 1 - Hello world/sketch.js
+++ week 1 - Hello world/sketch.js
@@ -56,8 +56,45 @@ function draw() {
   rect (237.5, 562.5, 37.5, 37.5);
   rect (312.5, 562.5, 37.5, 37.5);
  //chessboard
-  stroke(0)
-  strokeWeight(2)
-  rect(300,100,60,60)
+  stroke(0);
+  strokeWeight(2);
+  rect(300,100,60,60);
   triangle(300,100,330,50,360,100);
+  //osso
+  
+  noStroke(); 
+  fill(100);
+  rect(500,90,50,135);
+  rect(510,225,30,50);
+  stroke(0);
+  strokeWeight(1);
+  fill("green");
+  circle(525,205.5,35,35);
+  fill("orange");
+  circle(525,160.5,35,35);
+  fill("red");
+  circle(525,115.5,35,35);
+  // Traffic lights
+  strokeWeight(3);
+  fill("white");
+ rect(525,400,100,100,10);
+ fill(0);
+ circle(550,425,20,20,);
+ circle(575,450,20,20,);
+ circle(600,475,20,20,);
+ //dobbelsteen
+ noStroke();
+ fill("red");
+ rect(800,90,100,20);
+ rect(780,110,175,20);
+ fill(255,215,174);
+ rect(780,130,120,20);
+ rect(760,150,195,20);
+ rect(760,170,215,20);
+ rect(760,190,195,20);
+ rect(790,210,145,20);
+ fill("brown");
+ rect(780,130,60,20);
+ rect(760,150,20,20); rect(800,150,20,20);
+ rect(760,170,20,20);
 }
