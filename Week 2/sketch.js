bcebdf3eab4e8d17ce02a910af00e3e7f241a011
Evabrand10 <davelamberts2010@icloud.com>
2026-09-11 10:45:13 +0200

    UPDATE

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,5 +1,10 @@
 
-
+let Cloudx1 = 200;
+let Cloudx2 = 600;
+let Cloudx3 = 400;
+let CLoudspeed1 = +2.3
+let Cloudspeed2 = +2.1
+let Cloudspeed3 = +2.5
 
 
 function setup() {
@@ -8,12 +13,29 @@ function setup() {
 
 function draw() {
  background(53, 182, 246);
-  drawWolk(200, 145);
-  drawWolk(600, 100);
-  drawWolk(400,125);
+  drawWolk(Cloudx1, 145);
+  drawWolk(Cloudx2, 100); 
+  drawWolk(Cloudx3,125);
+  Cloudx1 = Cloudx1 + CLoudspeed1
+  Cloudx2 = Cloudx2 + Cloudspeed2
+  Cloudx3 = Cloudx3 + Cloudspeed3
+  if (Cloudx1 >= 850){
+    Cloudx1 = -50;
+    
+  }
+if (Cloudx2 >= 850){
+    Cloudx2 = -50;
+    
+  }
+  if (Cloudx3 >= 850){
+    Cloudx3 = -50;
+    
+  }
+   
+  //wolken
   
   fill ("yellow")
-  circle (200 + 1,100,50);
+  circle (200,100,50);
 
 }
 function drawWolk(xPos , yPos) {
@@ -26,5 +48,6 @@ function drawWolk(xPos , yPos) {
   circle(xPos, yPos + 5, 45);
   circle(xPos + 20, yPos, 55);
   circle(xPos + 50, yPos + 5, 45);
+  //wolk
 }
 
