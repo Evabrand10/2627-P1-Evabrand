306dc45d712a872764d084697dcd7154663d7d5c
Evabrand10 <davelamberts2010@icloud.com>
2026-09-11 09:59:54 +0200

    updateee

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,30 @@
+
+
+
+
 function setup() {
-  createCanvas(400, 400);
-}
+  createCanvas(800, 600);
+} 
 
 function draw() {
-  background(220);
+ background(53, 182, 246);
+  drawWolk(200, 145);
+  drawWolk(600, 100);
+  drawWolk(400,125);
+  
+  fill ("yellow")
+  circle (200 + 1,100,50);
+
 }
+function drawWolk(xPos , yPos) {
+  fill(220);
+  noStroke();
+  circle(xPos, yPos, 45);
+  circle(xPos + 20, yPos - 5, 55);
+  circle(xPos + 50, yPos, 45);
+  fill(255);
+  circle(xPos, yPos + 5, 45);
+  circle(xPos + 20, yPos, 55);
+  circle(xPos + 50, yPos + 5, 45);
+}
+
