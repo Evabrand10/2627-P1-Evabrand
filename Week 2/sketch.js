af65f6275c6ca4a1ce1d503e53689021e4f6ccf0
Evabrand10 <davelamberts2010@icloud.com>
2026-09-14 10:30:08 +0200

    Update sketch.js

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -2,9 +2,10 @@
 let Cloudx1 = 200;
 let Cloudx2 = 600;
 let Cloudx3 = 400;
-let CLoudspeed1 = +2.3
-let Cloudspeed2 = +2.1
-let Cloudspeed3 = +2.5
+let CLoudspeed1 = -2.3
+let Cloudspeed2 = -2.1
+let Cloudspeed3 = -2.5
+
 
 
 function setup() {
@@ -19,22 +20,24 @@ function draw() {
   
   fill("yellow");
   circle(200,100,50);
+  fill("gray");
+  triangle(350,500,450,150,650,500);
   drawWolk(Cloudx1, 145);
   drawWolk(Cloudx2, 100); 
   drawWolk(Cloudx3,125);
   Cloudx1 = Cloudx1 + CLoudspeed1
   Cloudx2 = Cloudx2 + Cloudspeed2
   Cloudx3 = Cloudx3 + Cloudspeed3
-  if (Cloudx1 >= 850){
-    Cloudx1 = -50;
+  if (Cloudx1 <= -50){
+    Cloudx1 = 850;
     
   }
-if (Cloudx2 >= 850){
-    Cloudx2 = -50;
+if (Cloudx2 <= -50){
+    Cloudx2 = 850;
     
   }
-  if (Cloudx3 >= 850){
-    Cloudx3 = -50;
+  if (Cloudx3 <= -50){
+    Cloudx3 = 850;
     
   }
   //wolken
@@ -46,12 +49,34 @@ if (Cloudx2 >= 850){
   rect(225,545,75,10,10);
   rect(375,545,75,10,10);
   rect(525,545,75,10,10);
-   rect(675,545,75,10,10);
+  rect(675,545,75,10,10);
+ 
+
+  
+  fill(70);
+  triangle(200,500,350,250,450,500);
+  fill(110)
+  triangle(550,500,650,350,950,500);
+  drawStoplight(600,340,575,315); 
   
   
  
 
 }
+function drawStoplight(xPos, yPos, xposrect ,yposrect) {
+  fill(100);
+  rect(xposrect, yposrect, 50, 135);
+  rect(xposrect+10, yposrect+135, 30, 50);
+  stroke(0);
+  strokeWeight(1);
+  fill(0,55,0);
+  circle(xPos, yPos+90, 35, 35);
+  fill(80, 50, 0);
+  circle(xPos, yPos+45, 35, 35);
+  fill(80,0,0);
+  circle(xPos, yPos, 35, 35);
+}
+//stopligt
 function drawWolk(xPos , yPos) {
   fill(220);
   noStroke();
