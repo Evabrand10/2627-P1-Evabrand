c284c1263c62bbdf31ae26c29d03942bef07b8b8
Evabrand10 <davelamberts2010@icloud.com>
2026-09-22 10:46:11 +0200

    Update sketch.js

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,6 +1,6 @@
-let playersturnR = 0
-let playersturnG = 100
-let playersturnB = 255
+let playersturnR = 255
+let playersturnG = 0
+let playersturnB = 0
 let rectW1 = 100
 let rectW2 = 100
 let rectW3 = 100
@@ -50,14 +50,61 @@ let playerturn = 0
 let playercolor = [playersturnR, playersturnG, playersturnB];
 function setup() {
   createCanvas(1400, 800);
+
 }
 
 function draw() {
-  background(playersturnR, playersturnG, playersturnB);
+  background(playercolor);
   fill(0);
   rect(475, 200, 400, 400, 10);
+  if (playerturn == 0) {
+   playercolor = [255,0,0]
+  }
+
+  else if (playerturn == 1) {
+    playercolor = [0,100,255]
+  }
 
 
+  
+
+  fill(colorchecker1);
+  rect(rectx1, rectY1, rectW1, rectH1, 10);//left top
+  fill(colorchecker2);
+  rect(rectx2, rectY2, rectW2, rectH2, 10);//middle top
+  fill(colorchecker3);
+  rect(rectx3, rectY3, rectW3, rectH3, 10);// right top
+  fill(colorchecker4);
+  rect(rectx4, rectY4, rectW4, rectH4, 10);//left middle
+  fill(colorchecker5);
+  rect(rectx5, rectY5, rectW5, rectH5, 10);//middle middle
+  fill(colorchecker6);
+  rect(rectx6, rectY6, rectW6, rectH6, 10);//right middle
+  fill(colorchecker7);
+  rect(rectx7, rectY7, rectW7, rectH7, 10);//left bottom
+  fill(colorchecker8);
+  rect(rectx8, rectY8, rectW8, rectH8, 10);//middle bottom
+  fill(colorchecker9);
+  rect(rectx9, rectY9, rectW9, rectH9, 10);//right bottom
+
+
+
+}
+
+function mousePressed(){
+  if(mouseButton == "left"){
+    handleClick();
+    playerturn = (playerturn + 1) 
+    if (playerturn == 2){
+      playerturn = 0
+    }
+   
+
+      
+  }
+}
+
+function handleClick(){
   if (mouseX > rectx1 && mouseX < rectx1 + rectW1 &&
     mouseY > rectY1 && mouseY < rectY1 + rectH1
   ) {
@@ -65,7 +112,10 @@ function draw() {
     rectH1 = 105
     rectW1 = 105
 
+
   }
+  
+
   else if (mouseX > rectx2 && mouseX < rectx2 + rectW2 &&
     mouseY > rectY2 && mouseY < rectY2 + rectH2
   ) {
@@ -124,15 +174,15 @@ function draw() {
     rectW9 = 105
   }
   else {
-    colorchecker1 = 220;
-    colorchecker2 = 220;
-    colorchecker3 = 220;
-    colorchecker4 = 220;
-    colorchecker5 = 220;
-    colorchecker6 = 220;
-    colorchecker7 = 220;
-    colorchecker8 = 220;
-    colorchecker9 = 220
+    colorchecker1 = 255;
+    colorchecker2 = 255;
+    colorchecker3 = 255;
+    colorchecker4 = 255;
+    colorchecker5 = 255;
+    colorchecker6 = 255;
+    colorchecker7 = 255;
+    colorchecker8 = 255;
+    colorchecker9 = 255
     rectH1 = 100
     rectW1 = 100
     rectH2 = 100
@@ -152,40 +202,7 @@ function draw() {
     rectH9 = 100
     rectW9 = 100
 
-    if (playerturn = 0) {
-      playersturnR = 255;
-      playersturnG = 0;
-      playersturnB = 0;
-    }
 
-    if (playerturn = 1) {
-      playersturnR = 0
-      playersturnG = 100;
-      playersturnB = 255;
-    }
 
   }
-
-  fill(colorchecker1);
-  rect(rectx1, rectY1, rectW1, rectH1, 10);//left top
-  fill(colorchecker2);
-  rect(rectx2, rectY2, rectW2, rectH2, 10);//middle top
-  fill(colorchecker3);
-  rect(rectx3, rectY3, rectW3, rectH3, 10);// right top
-  fill(colorchecker4);
-  rect(rectx4, rectY4, rectW4, rectH4, 10);//left middle
-  fill(colorchecker5);
-  rect(rectx5, rectY5, rectW5, rectH5, 10);//middle middle
-  fill(colorchecker6);
-  rect(rectx6, rectY6, rectW6, rectH6, 10);//right middle
-  fill(colorchecker7);
-  rect(rectx7, rectY7, rectW7, rectH7, 10);//left bottom
-  fill(colorchecker8);
-  rect(rectx8, rectY8, rectW8, rectH8, 10);//middle bottom
-  fill(colorchecker9);
-  rect(rectx9, rectY9, rectW9, rectH9, 10);//right bottom
-
-
-
 }
-
