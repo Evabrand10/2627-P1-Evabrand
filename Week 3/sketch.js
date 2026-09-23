6695205e914d2e72f9e9ac8ec1db06ccb230e01c
Evabrand10 <davelamberts2010@icloud.com>
2026-09-23 14:44:22 +0200

    update

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -51,24 +51,26 @@ let eigenaar7 = 0
 let eigenaar8 = 0
 let eigenaar9 = 0
 let playerturn = 1 // 0 = player 1 aan de beurt, 1 = player 2 is aan de beurt
-let playercolor = [0,100,255]
+let playercolor = [0, 100, 255]
 let reset = 0
-let win_top_left_right_blue = 0
-let win_middle_left_right_blue = 0
-let win_bottom_left_right_blue = 0
-let win_left_bottom_top_blue = 0
-let win_middle_bottom_top_blue = 0
-let win_right_bottom_top_blue = 0
-let win_diagonal_left_right_blue = 0
-let win_diagonal_right_left_blue = 0
-let win_top_left_right_red = 0
-let win_middle_left_rightred = 0
-let win_bottom_left_right_red = 0
-let win_left_bottom_top_red = 0
-let win_middle_bottom_top_red = 0
-let win_right_bottom_top_red = 0
-let win_diagonal_left_right_red = 0
-let win_diagonal_right_left_red = 0
+let win_top_left_right_blue = 5000
+let win_middle_left_right_blue = 5000
+let win_bottom_left_right_blue = 5000
+let win_left_bottom_top_blue = 5000
+let win_middle_bottom_top_blue = 5000
+let win_right_bottom_top_blue = 5000
+let win_diagonal_left_right_blue = 5000
+let win_diagonal_right_left_blue = 5000
+let win_top_left_right_red = 5000
+let win_middle_left_right_red = 5000
+let win_bottom_left_right_red = 5000
+let win_left_bottom_top_red = 5000
+let win_middle_bottom_top_red = 5000
+let win_right_bottom_top_red = 5000
+let win_diagonal_left_right_red = 5000
+let win_diagonal_right_left_red = 5000
+
+
 let start = 0
 let press_start = "press start"
 
@@ -82,6 +84,7 @@ function setup() {
 function draw() {
   background(playercolor);
   fill(0);
+  noStroke();
   rect(475, 200, 400, 400, 10);
   if (playerturn == 2) {
     playercolor = [255, 0, 0]
@@ -91,7 +94,30 @@ function draw() {
     playercolor = [0, 100, 255]
   }
 
-  
+  if (eigenaar1 == 2 && eigenaar2 == 2 && eigenaar3 == 2) {
+  win_top_left_right_red = 275
+  }
+  else {
+   win_top_left_right_red= 5000   
+  }
+  if (eigenaar4 == 2 && eigenaar5 == 2 && eigenaar6 == 2) {
+    win_middle_left_right_red = 400
+  }
+  else {
+    win_middle_left_right_red = 5000
+  }
+  if (eigenaar7 == 2 && eigenaar8 == 2 && eigenaar9 == 2) {
+    win_bottom_left_right_red = 525
+  }
+  else {
+    win_bottom_left_right_red = 5000
+  }
+    if (eigenaar1 == 2 && eigenaar4 == 2 && eigenaar7 == 2) {
+    win_left_bottom_top_red = 525
+  }
+  else {
+    win_bottom_left_right_red = 5000
+  }
 
   if (eigenaar1 == 0) {
     fill(255)
@@ -99,7 +125,7 @@ function draw() {
   else if (eigenaar1 == 1) {
     fill("blue")
   }
-  else {
+  else if (eigenaar1 == 2) {
     fill("red");
   }
   rect(rectx1, rectY1, rectW1, rectH1, 10);//left top
@@ -112,7 +138,7 @@ function draw() {
   else if (eigenaar2 == 1) {
     fill("blue")
   }
-  else {
+  else if (eigenaar2 == 2) {
     fill("red");
   }
   rect(rectx2, rectY2, rectW2, rectH2, 10);//middle top
@@ -133,7 +159,7 @@ function draw() {
   else if (eigenaar4 == 1) {
     fill("blue")
   }
-  else {
+  else if (eigenaar4 == 2) {
     fill("red");
   }
   rect(rectx4, rectY4, rectW4, rectH4, 10);//left middle
@@ -143,7 +169,7 @@ function draw() {
   else if (eigenaar5 == 1) {
     fill("blue")
   }
-  else {
+  else if (eigenaar5 == 2) {
     fill("red");
   }
   rect(rectx5, rectY5, rectW5, rectH5, 10);//middle middle
@@ -153,7 +179,7 @@ function draw() {
   else if (eigenaar6 == 1) {
     fill("blue")
   }
-  else {
+  else if (eigenaar6 == 2) {
     fill("red");
   }
   rect(rectx6, rectY6, rectW6, rectH6, 10);//right middle
@@ -163,8 +189,8 @@ function draw() {
   else if (eigenaar7 == 1) {
     fill("blue")
   }
-  else {
-    fill("red");
+  else if (eigenaar7 == 2) {
+    fill("red")
   }
   rect(rectx7, rectY7, rectW7, rectH7, 10);//left bottom
   if (eigenaar8 == 0) {
@@ -173,7 +199,7 @@ function draw() {
   else if (eigenaar8 == 1) {
     fill("blue")
   }
-  else {
+  else if (eigenaar8 == 2) {
     fill("red");
   }
   rect(rectx8, rectY8, rectW8, rectH8, 10);//middle bottom
@@ -183,27 +209,32 @@ function draw() {
   else if (eigenaar9 == 1) {
     fill("blue")
   }
-  else {
+  else if (eigenaar9 == 2) {
     fill("red");
   }
   rect(rectx9, rectY9, rectW9, rectH9, 10);//right bottom
   fill("yellow");
   rect(rectx10, rectY10, rectW10, rectH10, 10);//reset button
- 
- 
+
+
   fill(0)
   textSize(26)
-  text(press_start,1000,635)
- if (start == 0){
-  press_start = "press start"
- }
- else if (start >=1){
-  press_start = "reset"
- }
- 
- 
+  text(press_start, 1000, 635)
+  if (start == 0) {
+    press_start = "press start"
+  }
+  else if (start >= 1) {
+    press_start = "reset"
+  }
+  stroke(255);
+  strokeWeight(10)
+  line(400, win_top_left_right_red, 950, win_top_left_right_red);
+  line(400, win_top_left_right_blue, 950, win_top_left_right_blue);
+  line(400, win_middle_left_right_red, 950, win_middle_left_right_red);
+  line(400, win_bottom_left_right_red, 950, win_bottom_left_right_red);
+  line(550,200,550,300);
 }
- 
+
 
 
 
